377954a8581fac9fe5366ed927ca1a8550bad241
NatanJMai <natan.mai@hotmail.com>
2016-05-19 11:26:28 -0300

    Atualizando a leitura do arquivo

--- ler_entrada.py
+++ ler_entrada.py
@@ -1,9 +1,34 @@
 from estrutura import *
 
-arq = open("exemplo_1.in", "r")
+def abrir_arquivo(nome_arq):
+    return open(nome_arq, 'r')
 
-texto = arq.read()
 
-lista = texto.replace("\n", "").split(";")
+def ler_arquivo():
+    arq  = abrir_arquivo("exemplo_4.in")   
+    conf = []
 
-print (lista)
+    for i in arq:
+        if '*' in i: break
+
+        a = i.split(';')[:-1]
+        if a != []: conf.append(a)
+
+    for i in conf:
+        print(i)
+
+    #while 'E' not in rea :
+    #   rea = arq.read()
+    #   print(rea)
+
+
+#open("exemplo_1.in", "r")
+
+#texto = arq.read()
+
+#lista = texto.replace("\n", "").split(";")
+
+#print (lista)
+
+if __name__ == "__main__":
+    ler_arquivo()
