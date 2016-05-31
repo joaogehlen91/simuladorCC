f6d1a67e7b3f38bc0edc31c9d51e8ba98ed09078
João Gehlen <joaogehlen91@gmail.com>
2016-05-31 20:38:35 -0300

    explicacoes para tentar esclarecer

---
+++ lista.py
@@ -0,0 +1,26 @@
+lista = [x for x in xrange(10000)]
+
+
+print(lista)
+
+
+lista = sorted(lista)
+
+
+print(lista)
+
+lista.append(15)
+
+
+print(lista)
+
+lista = sorted(lista)
+
+print(lista)
+
+
+lista.append(1)
+
+lista = sorted(lista)
+
+print(lista)
