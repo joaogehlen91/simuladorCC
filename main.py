1813088dca66c3ba411860e78896c04ccaa93ec8
João Gehlen <joaogehlen91@gmail.com>
2016-06-14 20:44:09 -0300

    teste na main

--- main.py
+++ main.py
@@ -1,5 +1,28 @@
+import os
+import definicoes
+import ler_entrada
+from estrutura import Componente
 
-#lista_de_objetos = [c1, c2, r1, s1]
+ler_entrada.ler_arquivo('exemplo_4.in')
+os.system('clear')
+
+objetos = []
+
+for i in definicoes.lista_objetos:
+	objetos.append(i)
+
+#							   0  1  2   3   4   5   6
+#lista de objetos exemplo 4 = [E, S, C1, C2, I1, R1, R2]
+
+#faz as ligacoes entre os objetos
+objetos[0].next = objetos[2]
+objetos[2].next = objetos[5]
+objetos[4].next = objetos[3]
+objetos[3].next = objetos[6]
+
+for i in objetos:
+	if i.__class__ is Componente:
+		print(i.next)
 
 TS = 50000
 
