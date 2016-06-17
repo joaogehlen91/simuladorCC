d05dffa86ec572ee1b5b7af8f181f859d47b15dd
zanuzzorz <zanuzzorz@gmail.com>
2016-06-17 16:25:20 -0300

    atualização do teste3 (componente com multiplos servidores) e exemplo_1

--- teste3.py
+++ teste3.py
@@ -1,110 +1,87 @@
+# Um componente com multiplos servidores
+
 from estrutura import *
+from definicoes import *
+from ler_entrada import *
 
 entrada = [6, 24, 26, 35]
+ler_arquivo("exemplo_1.in")
 
-# Parte da configuracao
+objetos = []
+servidores = []
 
-serv1 = Servidor(1, 10, 35, 0, 0, False)                 #(id, ini, fim, ult_saida) Servidor
-serv2 = Servidor(2, 10, 20, 0, 0, False)                 #(id, ini, fim, ult_saida) Servidor
+for i in lista_objetos:
+  objetos.append(i)
+  print(i.nome)
 
-e  = Componente("E", None, None, None, None)  			  #Componente Entrada
-s  = Componente("S", None, None, None, None)  			  #Componente Saida
-c1 = Componente("C1", 2, [serv1, serv2], None, None)	#Componente 1 - Caixa com 2 servidores
+c1 = get_componente("C1")
+for i in c1.list_serv:
+  servidores.append(i)
 
-c1.list_serv.append(serv1)
+som  =  0
+aux = 0
+inc =  0
+x = 0
 
-e.saida    = c1  #E->C1
-c1.entrada = e	 #E<-C1
-
-s.entrada  = c1  #C1->S
-c1.saida   = s   #C1<-S
-
-# Fim da parte da configuracao
-
-servidor1 = c1.list_serv[0]
-servidor2 = c1.list_serv[1]
-
-
-# Um componente com dois servidor
-som   = 0
-aux   = 0
-inc   = 0
 saida = []
 espera = []
 atend = []
 
+full = False
+
 for i in entrada:
-  if inc < som and servidor1.atendimento == False and servidor2.atendimento == False: 
-    inc = som
+
+  inc = i
+
+  if x < len(servidores):
+    if servidores[x].atendimento == False:
+      servidores[x].atendimento = True
+      aux  = servidores[x].atividade()
+      atend.append(aux)
+      som  = inc + aux
+      saida.append(som)
+      x+=1
   else:
-   inc = i
+    full = True
 
-  if servidor1.atendimento == False and servidor2.atendimento == False or servidor1.atendimento == False and servidor2.atendimento == True:
-    servidor1.atendimento = True
-
-    aux  = servidor1.atividade()
-    atend.append(aux)
-    som  = inc + aux
-    saida.append(aux + inc) 
-
-  elif servidor1.atendimento == True and servidor2.atendimento == False:
-    servidor2.atendimento = True
-
-    aux  = servidor2.atividade()
-    atend.append(aux)
-    som  = inc + aux
-    saida.append(aux + inc)
-    
-  elif servidor1.atendimento == True and servidor2.atendimento == True:
-    espera.append(i)
+  if full == True:   #Todos servidores ocupados
+    for x in range(0, len(servidores)):
+      if servidores[x].atendimento == True:
+        espera.append(i)
+        break
 
 
-servidor1.atendimento = False
-servidor2.atendimento = False
+for x in range(0, len(servidores)):
+  servidores[x].atendimento = False
 
+
+x = 0
 for i in range(0, len(espera)):
+
   if espera[i] < saida[i]:
-    if servidor1.atendimento == False and servidor2.atendimento == False or servidor1.atendimento == False and servidor2.atendimento == True:
-      servidor1.atendimento = True
-      aux  = servidor1.atividade()
-      atend.append(aux)
-      som  = saida[i] + aux
-      saida.append(som)
-
-    elif servidor1.atendimento == True and servidor2.atendimento == False:
-      servidor2.atendimento = True
-      aux  = servidor2.atividade()
-      atend.append(aux)
-      som  = saida[i] + aux
-      saida.append(som)
-  else:
-    if inc < som and servidor1.atendimento == False and servidor2.atendimento == False: 
+    if inc < som:
       inc = som
+    if x < len(servidores):
+      if servidores[x].atendimento == False:
+        servidores[x].atendimento = True
+        aux  = servidores[x].atividade()
+        atend.append(aux)
+        som  = saida[i] + aux
+        saida.append(som)
+        x+=1
+  else:
+    inc = espera[i]
 
-    if servidor1.atendimento == False and servidor2.atendimento == False or servidor1.atendimento == False and servidor2.atendimento == True:
-      servidor1.atendimento = True
+    if x < len(servidores):
+      if servidores[x].atendimento == False:
+        servidores[x].atendimento = True
+        aux  = servidores[x].atividade()
+        atend.append(aux)
+        som  = inc + aux
+        saida.append(som) 
+        x+=1
 
-      aux  = servidor1.atividade()
-      atend.append(aux)
-      som  = inc + aux
-      saida.append(aux + inc) 
-
-    elif servidor1.atendimento == True and servidor2.atendimento == False:
-      servidor2.atendimento = True
-
-      aux  = servidor2.atividade()
-      atend.append(aux)
-      som  = inc + aux
-      saida.append(aux + inc)
-
-
-servidor1.atendimento = False
-servidor2.atendimento = False
-
-#entrada = ent
-  
 print("\nEntrada -> %s" % entrada)
 print("Tempo -> %s" % atend)
 print("Saida -> %s "% saida)
 print("\nEspera -> %s\n" % espera)
-
