9db3bf01e74e974b3f3b958357670997b49c2dbb
NatanJMai <natan.mai@hotmail.com>
2016-06-21 20:44:13 -0300

    Update mult servidores

--- multiplos_servidores.py
+++ multiplos_servidores.py
@@ -7,7 +7,7 @@ def zerarAtendimento(servs, num):
 def multiplos_servidores(nomeC):
    comp = df.get_componente(nomeC)
    etmp = comp.list_espera_entrada[0]
-   
+
    servidores = []
    for i in comp.list_serv:
      servidores.append(i)
@@ -18,6 +18,7 @@ def multiplos_servidores(nomeC):
    tam = len(servidores)
 
    for x in range(0, tam):
+     #if servidores[x].ult_saida < etmp -> Nao esta em atendimento
      if servidores[x].atendimento == False:
        servidores[x].atendimento = True
        aux  = servidores[x].atividade()
@@ -29,16 +30,22 @@ def multiplos_servidores(nomeC):
 
    if cont == tam:   #Todos servidores ocupados
      for x in range(0, tam):
+       #if servidores[x].ult_saida >= etmp -> Esta em atendimento
        if servidores[x].atendimento == True:
+         #aux1 = servidores[x].ult_saida
+         #aux = servidores[x].atividade()
+         #som = servidores[x].ult_saida + aux
          servidores[x].list_espera_entrada.append(etmp)
          comp.list_espera_entrada.pop(0)
          break
 
    #zerarAtendimento(servidores, 0)
 
+   print("Servidor-> %d" % x)
    print("Entrada -> %s" % etmp)
    print("Tempo -> %s" % aux)
    print("Saida -> %s \n"% som)
+
    return som
 
 
@@ -46,9 +53,29 @@ def multiplos_servidores(nomeC):
 if __name__ == '__main__':
    le.ler_arquivo("exemplo_1.in")
 
+   relogio = 0
    c = df.get_componente('C1')
-   c.list_espera_entrada = [2,5,20,30]
+   c.list_espera_entrada = []
 
-   while(c.list_espera_entrada):
-      multiplos_servidores('C1')
+   while relogio < 500:
+      if relogio == 2:
+         c.list_espera_entrada.append(2)
+         multiplos_servidores('C1')
+
+      if relogio == 5:
+         c.list_espera_entrada.append(5)
+         multiplos_servidores('C1')
+
+      if relogio == 20:
+         c.list_espera_entrada.append(20)
+         multiplos_servidores('C1')
+
+      if relogio == 40:
+         c.list_espera_entrada.append(40)
+         multiplos_servidores('C1')
+
+      relogio += 1
+
+
+
 # A main ira fazer isso.
