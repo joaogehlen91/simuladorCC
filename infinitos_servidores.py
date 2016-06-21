0420e9a506e7bf0c439038b9c18d4323a3d666d3
NatanJMai <natan.mai@hotmail.com>
2016-06-20 22:08:34 -0300

    Update ler_entrada para criar 1 servidor nos infinitos

--- infinitos_servidores.py
+++ infinitos_servidores.py
@@ -1,12 +1,18 @@
 import ler_entrada as le
 import definicoes  as df
 
-def infinitos_servidores(nomeC):
+def infinitos_servidores(nomeC, et):
    comp = df.get_componente(nomeC)
    serv = comp.list_serv[0] #Componente de infinitos servidores tem apenas 1 servidor.
-   df.imprime_objetos()
+
+   aux   = serv.atividade()
+   som   = aux + et
+
+   print("\nEntrada -> %s" % et)
+   print("Tempo -> %s" % aux)
+   print("Saida -> %s "% som)
 
 
 if __name__ == '__main__':
    le.ler_arquivo("exemplo_4.in")
-   infinitos_servidores('I1')
+   infinitos_servidores('I1', 8)
