da8de3f2cf4b7576ce6ec14de95002e2e80b4eb0
NatanJMai <natan.mai@hotmail.com>
2016-06-21 19:35:24 -0300

    Update roteadores

--- infinitos_servidores.py
+++ infinitos_servidores.py
@@ -1,18 +1,26 @@
 import ler_entrada as le
 import definicoes  as df
 
-def infinitos_servidores(nomeC, et):
+def infinitos_servidores(nomeC):
    comp = df.get_componente(nomeC)
+   etmp = comp.list_espera_entrada[0]
    serv = comp.list_serv[0] #Componente de infinitos servidores tem apenas 1 servidor.
 
    aux   = serv.atividade()
-   som   = aux + et
+   som   = aux + etmp
 
-   print("Entrada -> %s" % et)
+   print("Entrada -> %s" % etmp)
    print("Tempo -> %s" % aux)
    print("Saida -> %s "% som)
+   return som
 
 
+# A main ira fazer isso.
 if __name__ == '__main__':
    le.ler_arquivo("exemplo_4.in")
-   infinitos_servidores('I1', 8)
+
+   c = df.get_componente('I1')
+   c.list_espera_entrada = [2]
+
+   infinitos_servidores('I1')
+# A main ira fazer isso.
