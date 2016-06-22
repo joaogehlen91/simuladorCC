3b909f25dce9d81540be57d2a427e25baeb754d3
zanuzzorz <zanuzzorz@gmail.com>
2016-06-22 01:16:39 -0300

    update Multiplos Servidores

--- multiplos_servidores.py
+++ multiplos_servidores.py
@@ -1,9 +1,6 @@
 import ler_entrada as le
 import definicoes  as df
 
-def zerarAtendimento(servs, num):
-   servs[num].atendimento = False
-
 def multiplos_servidores(nomeC):
    comp = df.get_componente(nomeC)
    etmp = comp.list_espera_entrada[0]
@@ -16,38 +13,41 @@ def multiplos_servidores(nomeC):
    aux = 0
    som = 0
    tam = len(servidores)
+   ult_saida_menor = 9999999999999
 
    for x in range(0, tam):
-     #if servidores[x].ult_saida < etmp -> Nao esta em atendimento
-     if servidores[x].atendimento == False:
+     if servidores[x].ult_saida < etmp or servidores[x].atendimento == False: # Nao esta em atendimento (Algum servidor Livre Servidor)
        servidores[x].atendimento = True
        aux  = servidores[x].atividade()
        som  = etmp + aux
        comp.list_espera_entrada.pop(0)
+       servidores[x].ult_saida = som
+       serv = x
        break
      else:
        cont+=1
 
-   if cont == tam:   #Todos servidores ocupados
-     for x in range(0, tam):
-       #if servidores[x].ult_saida >= etmp -> Esta em atendimento
-       if servidores[x].atendimento == True:
-         #aux1 = servidores[x].ult_saida
-         #aux = servidores[x].atividade()
-         #som = servidores[x].ult_saida + aux
-         servidores[x].list_espera_entrada.append(etmp)
-         comp.list_espera_entrada.pop(0)
-         break
+   if cont == tam: # Servidores ocupados
 
-   #zerarAtendimento(servidores, 0)
+     for x in range(0, tam): # Procurar o menor tempo de saida
+       if servidores[x].ult_saida < ult_saida_menor:
+         ult_saida_menor = servidores[x].ult_saida
+         serv = x
 
-   print("Servidor-> %d" % x)
+     if ult_saida_menor >= etmp and servidores[serv].atendimento == True: # Esta em atendimento (Todos servidores ocupados no momento)
+       aux = servidores[serv].atividade()
+       som = ult_saida_menor + aux
+       servidores[serv].ult_saida = som
+
+       servidores[serv].list_espera_entrada.append(etmp)
+       comp.list_espera_entrada.pop(0)
+
+   print("Servidor -> %s "% serv)
    print("Entrada -> %s" % etmp)
    print("Tempo -> %s" % aux)
    print("Saida -> %s \n"% som)
 
    return som
-
 
 # A main ira fazer isso.
 if __name__ == '__main__':
@@ -70,12 +70,14 @@ if __name__ == '__main__':
          c.list_espera_entrada.append(20)
          multiplos_servidores('C1')
 
+      if relogio == 35:
+         c.list_espera_entrada.append(35)
+         multiplos_servidores('C1')
+
       if relogio == 40:
          c.list_espera_entrada.append(40)
          multiplos_servidores('C1')
 
       relogio += 1
-
-
 
 # A main ira fazer isso.
